ea76d5262ba922e196501636a7e274ba4ad68a49
Anastasia <potekh.anastasia@gmail.com>
2022-11-01 22:06:13 +0100

    2d commit

--- project1/protocol_parsing.py
+++ project1/protocol_parsing.py
@@ -5,10 +5,9 @@ import base64
 def decode(given_str):
 
     # decoding the string from base 64 to bytes
-    
     decoded = base64.b64decode(given_str)
 
-    # unpacking it into a tupleß
+    # unpacking it into a tuple
     unpacked = struct.unpack('2f',decoded)
 
 
@@ -16,9 +15,15 @@ def decode(given_str):
     temperature = unpacked[0]
     humidity = unpacked[1]
 
+    # print to check number
+    # print(temperature)
+    # print(humidity)
+
     # get rid of exponentioal and additional numbers after .
-    temperature = ("{:.2f}".format(temperature/10**-42))
-    humidity = ("{:.2f}".format(humidity/10**-42))
+
+
+    temperature = ("{:.2f}".format(temperature*10**-2))
+    humidity = ("{:.2f}".format(humidity*10**-2))
 
     print(f"Temperature is: {temperature} and Humidity is: {humidity}")
 
