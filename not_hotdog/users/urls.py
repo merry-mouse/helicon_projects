3d95a684eb6894dca28d064ac0c388ccd331ab15
stash <potekh.anastasia@gmail.com>
2022-11-29 16:57:04 +0100

    login required for ShowUserNameView

--- not_hotdog/users/urls.py
+++ not_hotdog/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
 from .views import SignUpView, CustomLoginView, ShowUserNameView
 
 
@@ -9,6 +10,6 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"), # custom view
     path("login/", CustomLoginView.as_view(), name="login"), # custom view
     path("logout/", LogoutView.as_view(), name="logout"), # Django built-in logout view
-    path("display_users/", ShowUserNameView.as_view(), name="display_users"), # listing registered users
+    path("display_users/", login_required(ShowUserNameView.as_view()), name="display_users"), # listing registered users
 ]
 
