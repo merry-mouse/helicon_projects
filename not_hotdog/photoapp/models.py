fe58b56a558b47a5b3c121c2dcaa09afa6d481bd
stash <potekh.anastasia@gmail.com>
2022-11-30 16:18:31 +0100

    added tags to Photo model

--- not_hotdog/photoapp/models.py
+++ not_hotdog/photoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
 
 
 class Photo(models.Model):
@@ -8,6 +9,7 @@ class Photo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='photos/')
     submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    tags = TaggableManager(blank=True) 
 
     def __str__(self):
         return self.title
