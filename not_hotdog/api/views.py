9801e192c7333f13d094fbd37e1423b175ce6438
stash <potekh.anastasia@gmail.com>
2022-12-05 14:47:18 +0100

    make views class based instead of function based

--- not_hotdog/api/views.py
+++ not_hotdog/api/views.py
@@ -1,22 +1,48 @@
 from rest_framework import status
- 
 from rest_framework.decorators import api_view
- 
 from rest_framework.response import Response
-
 from photoapp.models import Photo
+from .serializers import PhotoSerializer, PhotoDetailSerializer
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
  
-from .serializers import ContactSerializer
- 
-@api_view(['GET'])
- 
-def photo_list(request):
-    if request.method == 'GET':
- 
+
+class PhotoList(APIView):
+    """
+    List all photos, or create a new photo.
+    """
+    def get(self, request, format=None):
         photos = Photo.objects.all()
- 
-        serializer = ContactSerializer(photos, many=True)
- 
+        serializer = PhotoSerializer(photos, many=True)
         return Response(serializer.data)
- 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PhotoDetail(APIView):
+    """
+    Retrieve, update or delete a photo instance.
+    """
+    permission_classes = (IsAdminUser,)
+    def get_object(self, pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            raise Http404
+    
+
+    def get(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        serializer = PhotoSerializer(photo)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        serializer = PhotoDetailSerializer(photo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        photo = self.get_object(pk)
+        photo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
