b47af7bbd6f488812f32746d7258387206af3d11
stash <potekh.anastasia@gmail.com>
2022-12-15 16:41:32 +0100

    deleted unnecessary serializer

--- not_hotdog/api/serializers.py
+++ not_hotdog/api/serializers.py
@@ -1,34 +1,18 @@
 from rest_framework import serializers
 from taggit.serializers import TagListSerializerField, TaggitSerializer
- 
 from photoapp.models import Photo
  
+
 class PhotoSerializer(TaggitSerializer, serializers.ModelSerializer):
-
     tags = TagListSerializerField()
     
     class Meta:
- 
         model = Photo
-
-        fields = "__all__"
-
-
-class PhotoDetailSerializerGet(TaggitSerializer, serializers.ModelSerializer):
-    
-    tags = TagListSerializerField()
-    
-    class Meta:
-
-        model = Photo
-
         fields = "__all__"
 
 
 class PhotoDetailSerializerPutDelete(serializers.ModelSerializer):
  
     class Meta:
-
         model = Photo
-
         fields = ["id", "title", "not_hotdog_flag"]
