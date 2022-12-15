2e750e8db01f1a44b9e446689c9e0d63ea05cefe
stash <potekh.anastasia@gmail.com>
2022-12-15 16:43:11 +0100

    override save f-on in the model, send msg rabbitmq

--- not_hotdog/photoapp/models.py
+++ not_hotdog/photoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from taggit.managers import TaggableManager
+from api.send import send_message
 
 
 class Photo(models.Model):
@@ -12,7 +13,19 @@ class Photo(models.Model):
     tags = TaggableManager(blank=True) 
     not_hotdog_flag = models.BooleanField(null=True) # if not a hotdog = True
 
+
     def __str__(self):
         return self.title
 
 
+    def save(self, *args, **kwargs):
+        # photo = super().save(force_insert, force_update, using, update_fields)
+        # send_message({'photo_id': photo.id})
+        # return photo
+        photo = super().save(self)
+        send_message("New image uploaded", {
+            "id": self.id,
+            "title": self.title,
+            "image": self.image.path
+        })
+        return photo
