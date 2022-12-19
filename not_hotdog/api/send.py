e0d92b90784736d760d6f5cbc6d4a7b8c9ba6642
stash <potekh.anastasia@gmail.com>
2022-12-19 17:29:51 +0100

    removed unnecessary lines in sender

--- not_hotdog/api/send.py
+++ not_hotdog/api/send.py
@@ -4,14 +4,6 @@ import pika, time, json
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-# channel.queue_declare(queue='hello')
-# for i in range(10):
-#     channel.basic_publish(exchange='',
-#     routing_key='hello',
-#     body=f'Hello from Helicon!, message {i}')
-#     print(f"Sent 'Hello from Helicon! message {i}'")
-#     time.sleep(2)
-# connection.close()
 
 # method parameter is the information about a message and body is the message to be sent
 def send_message(method, body): # handles the sending of the message
