606793498191e8ec2ba4148bba39798291d4ef8f
stash <potekh.anastasia@gmail.com>
2022-12-21 14:43:59 +0100

    changed receiver, it now uses model to change flag

--- not_hotdog/api/receive.py
+++ not_hotdog/api/receive.py
@@ -4,6 +4,9 @@ import pika
 import django
 from sys import path
 from os import environ
+import requests
+import time
+
 
 path.append('/Users/stash/Desktop/my_projects/not_hotdog/') # path to settings.py file
 # need to set up DJANGO_SETTINGS_MODULE because we are accessing a model while outside the main app
@@ -21,21 +24,47 @@ channel.queue_declare(queue='photos')
 # properties are user-defined properties on the message
 # body is the message received.
 def callback(ch, method, properties, body):
-    print("Received in photos...")
+    print("\nReceived in photos...")
     data = json.loads(body)
-    print(data)
-    id, title = data["id"], data["title"]
-    check_if_hotdog_in_name(id, title)
+    id, image_path = data["id"], data["image"]
+    use_higgingface_model_to_check_if_hotdog(id, image_path)
 
-def check_if_hotdog_in_name(image_id, image_title):
-    image_object = Photo.objects.get(id=image_id)
-    if "hotdog" in image_title.lower():
-        print("Found word hotdog in name!")
-        image_object.not_hotdog_flag = False
-        image_object.save()
-    else:
-        image_object.not_hotdog_flag = True
-        image_object.save()
+
+def use_higgingface_model_to_check_if_hotdog(id, path_to_uploaded_image):
+    # connect to the higgung face api
+    API_URL = "https://api-inference.huggingface.co/models/julien-c/hotdog-not-hotdog"
+    headers = {"Authorization": "Write authorization key here"}
+    
+    # getting the reulting scores from the model
+    try:
+        with open(path_to_uploaded_image, "rb") as f:
+            data = f.read()
+        response = requests.request("POST", API_URL, headers=headers, data=data)
+        time.sleep(2) # wait 2 sec for model to download
+        scores_list_of_dicts = json.loads(response.content.decode("utf-8"))
+        # separating scores values from the received list
+        hotdog_score = scores_list_of_dicts[0]['score'] # probability, float value from 0 to 1
+        not_hotdog_score = scores_list_of_dicts[1]['score'] # probability, float value from 0 to 1
+        print("\nHotdog score = ", hotdog_score, "\n","NOT Hotdog score = ", not_hotdog_score)
+
+        # changing not_hotdog_flag depending on the model score
+        image_object = Photo.objects.get(id=id)
+        probability_border_value = 0.7 # we agreed to count is as a hotdog if hotdog score is more than 70%
+        if hotdog_score > probability_border_value:
+            print("\nHotdog score was higher than 70%, setting image not_hotdog_flag to False.")
+            image_object.not_hotdog_flag = False
+            image_object.save()
+            print("\nChanged not_hotdog_flag to False, It is a hotdog.")
+        else:
+            print("\nHotdog score is too low, setting image not_hotdog_flag to True.\n")
+            image_object.not_hotdog_flag = True
+            image_object.save()
+            print("\nChanged not_hotdog_flag to True, it is not a hotdog")
+        print("---------------------------------------------------")
+
+    except KeyError:
+        print("Key Error happened, not_hotdog_flag wasn't changed")
+
 
 channel.basic_consume(queue='photos', on_message_callback=callback, auto_ack=True)
 print("Started Consuming...")
