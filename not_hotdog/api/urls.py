0ac195fd5c9623bfbf8ea40d81c6508c1406859d
stash <potekh.anastasia@gmail.com>
2022-12-21 14:45:28 +0100

    added comments in api urls

--- not_hotdog/api/urls.py
+++ not_hotdog/api/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 
-    path('photos/', views.PhotoList.as_view()),
+    path('photos/', views.PhotoList.as_view()), # to see all listed photos
 
-    path('get_detail/<int:pk>/', views.PhotoDetailGetView.as_view()),
+    path('get_detail/<int:pk>/', views.PhotoDetailGetView.as_view()), # to see the detail of one photo
 
-    path('update_detail/<int:pk>/', views.PhotoDetailPutDeleteView.as_view()),
+    path('update_detail/<int:pk>/', views.PhotoDetailPutDeleteView.as_view()), # to change the detail
 ]
